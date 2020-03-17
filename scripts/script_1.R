6f2e7b3b293e09c0576f8a0e3986793bb0f07c38
Fiona84 <fiona_17@interia.pl>
2020-03-17 12:46:16 +0100

    KW 2020-03-17

--- scripts/script_1.R
+++ scripts/script_1.R
@@ -1,8 +1,10 @@
 #włączenie bibliotek
 library(tm)
+library(hunspell)
+library(stringr)
 
 #zmiana katalogu roboczego
-workDir <- "D:\\KW\\PJN\\TextMining12S"
+workDir <- "F:\\KW\\TextMining12S"
 setwd(workDir)
 
 #definicja katalogów projektu
@@ -50,4 +52,46 @@ stoplist <- readLines(
 corpus <- tm_map(corpus, removeWords, stoplist)
 corpus <- tm_map(corpus, stripWhitespace)
 
-writeLines(as.character(corpus[[1]]))
+remove_char <- content_transformer(
+  function(x, pattern, replacement) 
+    gsub(pattern, replacement, x)
+)
+
+#usunięcie "em dash" i 3/4 z tekstów
+corpus <- tm_map(corpus, remove_char, intToUtf8(8722), "")
+corpus <- tm_map(corpus, remove_char, intToUtf8(190), "")
+
+#lematyzacja - sprowadzenie do formy podstawowej
+polish <- dictionary(lang = "pl_PL")
+
+lemmatize <- function(text) {
+  simple_text <- str_trim(as.character(text[1]))
+  parsed_text <- strsplit(simple_text, split = " ")
+  new_text_vec <- hunspell_stem(parsed_text[[1]], dict = polish)
+  for (i in 1:length(new_text_vec)){
+    if (length(new_text_vec[[i]]) == 0) new_text_vec[i] <- parsed_text[[1]][i]
+    if (length(new_text_vec[[i]]) > 1) new_text_vec[i] <- new_text_vec[[i]][1]
+  }
+  new_text <- paste(new_text_vec, collapse = " ")
+  return(new_text)
+}
+
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+#usunięcie rozszerzeń z nazw dokumentów
+cut_extensions <- function(document) {
+  meta(document, "id") <- gsub(pattern = "\\.txt$", "", meta(document, "id"))
+  return(document)
+}
+
+corpus <- tm_map(corpus, cut_extensions)
+
+#eksport korpusu przetworzonego do plików tekstowych
+preprocessed_dir <- paste(
+  outputDir,
+  "\\",
+  "Literatura - streszczenia - przetworzone",
+  sep = ""
+)
+dir.create(preprocessed_dir, showWarnings = FALSE)
+writeCorpus(corpus, path = preprocessed_dir)
